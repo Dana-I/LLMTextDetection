294637bdc369a72c056fa2e1a4f86051617189a5
Dana Ibrahim <idana693@gmail.com>
2025-03-02 14:43:54 -0600

    Added function for extracting logprobs of tokens and calculating the average logprob per token

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # LLMTextDetection
+
+# Logprob is the logarithm of the probability of a token being predicted by the model.
+# - higher probability: the more confident the model is about the token being the correct next word 
+# - logprob closer to zero --> high probability 
+# - logprob closer to large negative numbers --> low probability
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append("../")
 from dotenv import load_dotenv
 
-def grok_chat_completion(content, model="grok-2-latest", stream=False, temperature=0):
+def grok_chat_completion(content, model="grok-2-latest", stream=False, temperature=0, logprobs=True):
     load_dotenv()
     api_key = os.getenv("GROK_2_API_KEY")
     url = "https://api.x.ai/v1/chat/completions"
@@ -25,13 +25,46 @@ def grok_chat_completion(content, model="grok-2-latest", stream=False, temperatu
         ],
         "model": f"{model}",
         "stream": stream,
-        "temperature": temperature
+        "temperature": temperature,
+        "logprobs": logprobs
     }
     response = requests.post(url, headers=headers, json=data)
     return response.json()
+
+# Extracts log probabilities from response and computes their average.
+def extract_logprobs(response):
+    choices = response.get("choices", [])
+
+    if not choices:
+        print("No choices returned in response.")
+        return None
+    
+    first_choice = choices[0] 
+    logprobs = first_choice.get("logprobs", {}) # gets logprobs of first choice
+
+    if not logprobs or "content" not in logprobs:
+        print("No logprobs returned in response.")
+        return None
+    
+    token_logprob = [entry["logprob"] for entry in logprobs["content"] if "logprob" in entry] # gets logprob of each token
+
+    if not token_logprob:
+        print("No logprob returned in reponse.")
+        return None
+    
+    avg_logprob = sum(token_logprob) / len(token_logprob) 
+    return avg_logprob
+
 
 if __name__ == "__main__":
     content = "What is the weather like today in DFW?"
     response = grok_chat_completion(content)
     print(response)
+
+    avg_logprob = extract_logprobs(response)  # Extract log probabilities
+    if avg_logprob is not None:
+        print(f"Average log probability: {avg_logprob}")
+    else:
+        print("Failed to calculate average log probability.")
+
     
